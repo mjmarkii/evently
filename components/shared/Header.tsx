a324bf1a41b1e06b980d9f0c176487f3e5dc5ea4
Mark Jerly Bundalian <mbundalian@fullscale.ph>
2024-03-16 22:10:35 +0800

    Develop mobile nav

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,8 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { Button } from "../ui/button"
+import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -11,10 +13,17 @@ const Header = () => {
           <Image src="/assets/images/logo.svg" alt="Evently Logo" width={128} height={38} />
         </Link>
 
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           {/* Will show if signed in */}
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
 
           {/* will only show if signed out */}
