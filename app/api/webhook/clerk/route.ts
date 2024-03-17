672cab64a7667dda2f131a8bcc988a08d43b50fa
Mark Jerly Bundalian <mbundalian@fullscale.ph>
2024-03-18 00:29:43 +0800

    update main

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,30 +54,29 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
  
-  if (eventType === 'user.created') {
+  if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-        clerkId: id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name,
-        lastName: last_name,
-        photo: image_url,
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
     }
 
-    // create user
     const newUser = await createUser(user);
 
-    if (newUser) {
+    if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
-          userId: newUser._id,
+          userId: newUser._id
         }
-      });
+      })
     }
 
-    return NextResponse.json({ message: 'OK', user: newUser });
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
   if (eventType === 'user.updated') {
