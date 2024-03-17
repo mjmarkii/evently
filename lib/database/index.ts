433bf30c23fa99c764fb9a099873853de044d859
Mark Jerly Bundalian <mbundalian@fullscale.ph>
2024-03-18 00:31:32 +0800

    Update db conn

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,4 +1,3 @@
-// cache db connection across multiple invocations
 import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -6,24 +5,16 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-    // check if there is already a cached connection
-    if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) {
-        throw new Error(
-            'Please define the MONGODB_URI environment variable inside .env.local'
-        );
-    }
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    // Otherwise create a new one
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    });
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    cached.conn = await cached.promise;
+  cached.conn = await cached.promise;
 
-    return cached.conn;
+  return cached.conn;
 }
-
-// Server actions
