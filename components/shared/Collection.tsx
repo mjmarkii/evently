989dfc13ebff27d19382045cd49c7858862a70f2
Mark Jerly Bundalian <mbundalian@fullscale.ph>
2024-03-24 21:20:41 +0800

    Create card component

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -37,7 +37,7 @@ const Collection = ({
                   <Card event={event} hasOrderLink={hasOrderLink} hidePrice={hidePrice} />
                 </li>
               );
-            }}
+            })}
           </ul>
         </div>
       ) : (
