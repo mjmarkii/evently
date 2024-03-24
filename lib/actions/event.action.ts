cabaadb209750786f28ba76fd2f2d6df6474f653
Mark Jerly Bundalian <mbundalian@fullscale.ph>
2024-03-24 19:28:32 +0800

    Create event details page

--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -1,3 +1,4 @@
+import { model } from 'mongoose';
 "use server";
 
 import { connectToDatabase } from '../database';
@@ -5,6 +6,14 @@ import Event from '../database/models/event.model';
 import User from '../database/models/user.model';
 import { handleError } from '../utils';
 import { CreateEventParams } from './../../types/index';
+import Category from '../database/models/category.model';
+
+// populate the event with the relationship tables
+const populateEvent = async (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' });
+}
 
 export const createEvent = async ({ event, userId, path} : CreateEventParams) => {
     try {
@@ -23,6 +32,22 @@ export const createEvent = async ({ event, userId, path} : CreateEventParams) =>
         });
 
         return JSON.parse(JSON.stringify(newEvent));
+    } catch (error) {   
+        handleError(error);
+    }
+}
+
+export const getEventById = async(eventId: string) => {
+    try {
+        await connectToDatabase();
+
+        const event = await populateEvent(Event.findById(eventId));
+
+        if (!event) {
+            throw new Error('Event not found');
+        }   
+
+        return JSON.parse(JSON.stringify(event));
     } catch (error) {   
         handleError(error);
     }
