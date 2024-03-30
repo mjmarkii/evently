1293e96b8eac2c3ff3a0fb5f2ca241ca41309378
Mark Jerly Bundalian <mbundalian@fullscale.ph>
2024-03-30 10:18:40 +0800

    Implement UI for ticketing

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from '@/components/shared/CheckoutButton';
 import Collection from '@/components/shared/Collection';
 import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.action'
 import { formatDateTime } from '@/lib/utils';
@@ -46,6 +47,7 @@ const EventDetails = async ({ params: { id }, searchParams} : SearchParamProps) 
             </div>
 
             {/* CHECKOUT BUTTON */}
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
